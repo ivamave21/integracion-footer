e9b0e0d5ded0f5cda22ea6d21688ffbc87ae42bf
Santiago Vargas <santiagovargasve@gmail.com>
2020-10-18 17:40:02 -0500

    Ya esta el slider creado y funcional

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#lista-productos").lightSlider({
+        item:1,
+        auto:true,
+        loop:true,
+        speed:1000,
+        stop:2000,
+    });
+});
